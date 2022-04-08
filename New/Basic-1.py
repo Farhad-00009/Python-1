5fa0615b6e93b65cd02cd2f582aa85455c50f3c8
Farhad-009 <61062795+Farhad-00009@users.noreply.github.com>
2022-04-08 16:14:24 +0600

    Add files via upload

---
+++ New/Basic-1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Basic First Program 
+# 
+
+# In[15]:
+
+
+print("this Is My first Program")
+
+
+# In[16]:
+
+
+print("This My secend Program")
+
+
+# In[23]:
+
+
+print("Name:Abu Noman Farhad  ID:UG02-48-18-009 Phone :01917049993 gmail:ahamedfarhad1@gmail.com ")
+
+
+# In[ ]:
+
+
+
+
